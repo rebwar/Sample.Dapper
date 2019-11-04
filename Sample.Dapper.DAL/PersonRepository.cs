2ef0a1a5978fc7b8a37ab201b5602871cd4d92f7
Rebwar Hosain Poori <Rebwar.poori@hotmail.com>
2019-11-04 11:00:16 +0330

    Add project files.

---
+++ Sample.Dapper.DAL/PersonRepository.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using Sample.Dapper.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace Sample.Dapper.DAL
+{
+    public class PersonRepository
+    {
+        private readonly IDbConnection dbConnection;
+
+        public PersonRepository(IDbConnection dbConnection)
+        {
+            this.dbConnection = dbConnection;
+        }
+        public List<Person> GetPeople()
+        {
+           var people= dbConnection.Query<Person>("select * from person").ToList();
+            return people;
+        }
+    }
+}
---
+++ Sample.Dapper.Entities/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Sample.Dapper.Entities
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+    }
+}
