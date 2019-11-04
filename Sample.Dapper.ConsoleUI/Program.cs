08dbb2944d8a05f132bdda831545eb1621049618
Rebwar Hosain Poori <Rebwar.poori@hotmail.com>
2019-11-04 11:22:49 +0330

    Create Console Ui and get all people by Dapper

---
+++ Sample.Dapper.ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using Sample.Dapper.DAL;
+using System;
+using System.Data.SqlClient;
+
+namespace Sample.Dapper.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conn = new SqlConnection(@"server=(localdb)\MSSQLLocalDB;initial catalog=DapperSampleDb;integrated security=true");
+            var repo = new PersonRepository(conn);
+            var people = repo.GetPeople();
+            foreach (var item in people)
+            {
+                Console.WriteLine($"Id={item.PersonId} \t Name={item.Name} \t Family={item.Family}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
